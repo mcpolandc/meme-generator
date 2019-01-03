b577576c1f8fed6b960a9fc667337b4b8ddb76ba
mcpolandc <mcpolandc@users.noreply.github.com>
2019-01-03 20:15:43 +0000

    Load images from imgflip API

--- src/components/image-grid/index.tsx
+++ src/components/image-grid/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import axios from "axios";
 import {
   GridList,
   GridListTile,
@@ -8,20 +9,48 @@ import {
   InfoIcon
 } from "@material-ui/core";
 
-import { Imgs, ImgFolder, ImgExt } from "./constants";
+import { GetImages } from "./actions";
 
-const ImageGrid = () => (
-  <GridList cellHeight={180}>
-    <GridListTile key="Subheader" cols={12} style={{ height: "auto" }}>
-      <ListSubheader component="div">Choose a meme</ListSubheader>
-    </GridListTile>
-    {Imgs.map(img => (
-      <GridListTile>
-        <img src={img.url} />
-        <GridListTileBar title={img.name} />
-      </GridListTile>
-    ))}
-  </GridList>
-);
+const memeEndpoint = "https://api.imgflip.com/get_memes";
+
+class ImageGrid extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { imgs: [] };
+  }
+
+  async componentDidMount() {
+    let resp;
+
+    try {
+      resp = await axios.get(memeEndpoint);
+    } catch (err) {
+      console.log(err);
+    }
+
+    const imgs = resp.data.data.memes;
+
+    this.setState({ imgs });
+  }
+
+  render() {
+    const { imgs } = this.state;
+
+    return (
+      <GridList cellHeight={180}>
+        <GridListTile key="Subheader" cols={12} style={{ height: "auto" }}>
+          <ListSubheader component="div">Choose a meme</ListSubheader>
+        </GridListTile>
+        {imgs.map(img => (
+          <GridListTile>
+            <img src={img.url} />
+            <GridListTileBar title={img.name} />
+          </GridListTile>
+        ))}
+      </GridList>
+    );
+  }
+}
 
 export default ImageGrid;
